88e7a72033680a2c9575f1b84e564909c258a90e
PasitJong <jong.pasitchote@gmail.com>
2026-09-01 23:01:23 +0700

    Link line auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,17 @@
-import { getLineAccessToken } from "../services/line.service.js";
+import {
+    getLineAccessToken,
+    getLineProfile
+} from "../services/line.service.js";
 
 export const lineCallback = async (req, res) => {
+    // console.log("callback")
+    // console.log("Query:", req.query)
+
     try {
+
         const { code, state } = req.query;
+        // console.log("Test Code:", code)
+        // console.log("Callback:", process.env.LINE_CALLBACK_URL);
 
         if (!code) {
             return res.status(400).json({
@@ -11,9 +20,10 @@ export const lineCallback = async (req, res) => {
         }
 
         const tokenData = await getLineAccessToken(code);
+        // console.log("Token:", tokenData)
 
         const profile = await getLineProfile(tokenData.access_token);
-        console.log("profile from authcontroller:", profile)
+        // console.log("profile from authcontroller:", profile)
 
 
         return res.json({
@@ -30,7 +40,7 @@ export const lineCallback = async (req, res) => {
             error.response?.data || error.message
         );
         return res.status(500).json({
-            message:"LINE authentication failed"
+            message: "LINE authentication failed"
         });
     }
 };
--- src/services/line.service.js
+++ src/services/line.service.js
@@ -9,20 +9,24 @@ export const getLineAccessToken = async (code) => {
     new URLSearchParams({
       grant_type: "authorization_code",
       code,
-      redirect_url: process.env.LINE_CALLBACK_URL,
+      redirect_uri: process.env.LINE_CALLBACK_URL,
       client_id: process.env.LINE_CHANNEL_ID,
       client_secret: process.env.LINE_CHANNEL_SECRET,
     }),
     {
-      headers: { "Contect-Type": "application/x-www-form-urlencoded", }
+      headers: { "Content-Type": "application/x-www-form-urlencoded", }
     }
+
   );
   return response.data;
+  
 };
 
-export const getLineProfile = async(accessToken)=>{
-  const response = await axios.get(LINE_PROFILE_URL,{
-    headers:{Authorization:`Bearer ${accessToken}`}
+export const getLineProfile = async (accessToken) => {
+  const response = await axios.get(LINE_PROFILE_URL, {
+    headers: { Authorization: `Bearer ${accessToken}` }
   });
   return response.data;
 }
+
+export const getLineUser = async()
