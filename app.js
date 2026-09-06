419183d4c4d00c7b0b95d4f317c3b219aa8a89c8
supanatchrn <supanat.char@gmail.com>
2026-09-06 13:54:26 +0700

    feat: implement OCR auto-extraction and bill verification workflow

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
 import express from 'express';
 import cors from "cors";
-import createHttpError from 'http-errors'
-
+import createHttpError from 'http-errors';
+import path from 'path';
 
 import authRoute from './src/routes/auth.routes.js';
 import errorMiddleware from './src/middlewares/error.middleware.js';
 import billRoute from './src/routes/bill.routes.js';
 import ocrRoute from "./src/routes/ocr.routes.js";
 
-const app = express()
+const app = express();
 
 app.use(cors({
   origin: "http://localhost:5173"
 }));
 
+app.use(express.json());
 
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
-app.use(express.json())
+app.use('/api/auth', authRoute);
+app.use('/api/bills', billRoute);
+app.use('/api/ocr', ocrRoute);
 
-app.use('/api/auth', authRoute)
-app.use('/api/bill', billRoute)
-app.use('/api/ocr', ocrRoute)
-
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.json({
-    message:'WongLao API is running'
-  })
-})
+    message: 'WongLao API is running'
+  });
+});
 
 app.use((req, res, next) => {
-  return next(createHttpError.NotFound())
-})
-
+  return next(createHttpError.NotFound());
+});
 
 app.use(errorMiddleware);
 
